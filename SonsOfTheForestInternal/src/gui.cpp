cf2d3b19aa09fba85cb9e30d984a9e2ea84c5a87
SpookySec <sp00kys3c@gmail.com>
2023-03-29 22:25:24 +0300

    UPDATE UPDATE

--- SonsOfTheForestInternal/src/gui.cpp
+++ SonsOfTheForestInternal/src/gui.cpp
@@ -3,7 +3,60 @@
 #include "../hdr/config.hpp"
 #include "../hdr/hax.hpp"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "../ext/std_image.h"
+
 extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int image_width = 0;
+int image_height = 0;
+ID3D11ShaderResourceView* texture = NULL;
+
+bool LoadTextureFromFile(const char* filename, ID3D11ShaderResourceView** out_srv, int* out_width, int* out_height)
+{
+    // Load from disk into a raw RGBA buffer
+    int image_width = 0;
+    int image_height = 0;
+    unsigned char* image_data = stbi_load(filename, &image_width, &image_height, NULL, 4);
+    if (image_data == NULL)
+        return false;
+
+    // Create texture
+    D3D11_TEXTURE2D_DESC desc;
+    ZeroMemory(&desc, sizeof(desc));
+    desc.Width = image_width;
+    desc.Height = image_height;
+    desc.MipLevels = 1;
+    desc.ArraySize = 1;
+    desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    desc.SampleDesc.Count = 1;
+    desc.Usage = D3D11_USAGE_DEFAULT;
+    desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+    desc.CPUAccessFlags = 0;
+
+    ID3D11Texture2D* pTexture = NULL;
+    D3D11_SUBRESOURCE_DATA subResource;
+    subResource.pSysMem = image_data;
+    subResource.SysMemPitch = desc.Width * 4;
+    subResource.SysMemSlicePitch = 0;
+    Globals::Gui::pDevice->CreateTexture2D(&desc, &subResource, &pTexture);
+
+    // Create texture view
+    D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
+    ZeroMemory(&srvDesc, sizeof(srvDesc));
+    srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+    srvDesc.Texture2D.MipLevels = desc.MipLevels;
+    srvDesc.Texture2D.MostDetailedMip = 0;
+    Globals::Gui::pDevice->CreateShaderResourceView(pTexture, &srvDesc, out_srv);
+    pTexture->Release();
+
+    *out_width = image_width;
+    *out_height = image_height;
+    stbi_image_free(image_data);
+
+    return true;
+}
 
 void InitGui()
 {
@@ -17,10 +70,9 @@ void InitGui()
     ImGuiIO& io = ImGui::GetIO();
     io.ConfigFlags = ImGuiConfigFlags_NoMouseCursorChange;
     //io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
     io.IniFilename = nullptr;
     io.LogFilename = nullptr;
-    io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\Bedstead.ttf", 18.f);
-    io.Fonts->AddFontDefault();
     ImGui_ImplWin32_Init(Globals::Gui::window);
     ImGui_ImplDX11_Init(Globals::Gui::pDevice, Globals::Gui::pContext);
 
